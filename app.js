c3387220c01c5f296c23585ebc7ff4aab713c9de
donzazi <ellyas20@live.dk>
2023-10-04 12:04:10 +0200

    Opgave 1.5 fuldført!

--- app.js
+++ app.js
@@ -7,15 +7,51 @@ console.log(
 console.log("Hello World");
 
 console.log(
-  "opgave 1.2 - Lav et program, som skriver “Hello” og på næste linje skriver “World” i konsollen")
+  "opgave 1.2 - Lav et program, som skriver “Hello” og på næste linje skriver “World” i konsollen"
+);
+console.log("Hello\nWorld");
+console.log("Hello");
+console.log("World");
 
-console.log("Hello\nWorld" )
-console.log("Hello")
-console.log("World")
+console.log("opgave 1.3 - Lav et program, som skriver følgende i konsollen");
+
+console.log("*\n**\n***\n**\n*\n");
 
 console.log(
-  "opgave 1.3 - Lav et program, som skriver følgende i konsollen")
+  "opgave 1.4 - Lav et program, hvor i definere en konstant eller variabel og programmet skriver hvilken type det er, i konsollen. (Altså om det er en int, string, bool osv.)"
+);
 
-  console.log(
-    "*\n**\n***\n**\n*\n"
-  )
+console.log(typeof "foo");
+console.log(typeof 42);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+//let foo = 42; foo is now a number
+//foo = "bar"; foo is now a string
+//foo = true;  foo is now a boolean
+
+//const fuu = 42; // fuu is a number
+//const result = fuu + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+
+//Type	      typeof return value	    Object wrapper
+
+//Null	      "object"	              N/A
+//Undefined	  "undefined" 	          N/A
+//Boolean   	"boolean"	              Boolean
+//Number	    "number"	              Number
+//BigInt	    "bigint"	              BigInt
+//String	    "string"	              String
+//Symbol	    "symbol"	              Symbol
+
+console.log(
+  "Opgave 1.5 - Print svarene på følgende matematiske operationer ved at skrive det direkte ind i Console.log(): fx, Console.log(5 + 5) ⇒ 10"
+);
+
+console.log(10 - 5);
+console.log(25 / 3);
+console.log(25 % 3);
+console.log(25 % 2);
+console.log(3 ** 2);
+
+console.log("Opgave ");
